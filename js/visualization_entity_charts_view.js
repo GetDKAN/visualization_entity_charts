798f781b0ebc6913230b1c1f22503dcae8ff0ce8
Andrés Díaz Soto <andres@estudiomanati.com>
2015-07-22 20:02:26 -0600

    Added code to remove '$' and '%' from records before rendering the charts.

--- js/visualization_entity_charts_view.js
+++ js/visualization_entity_charts_view.js
@@ -36,6 +36,17 @@
         var graph = null;
 
         dataset.fetch().done(function(dataset){
+
+          // Remove '$' and '%' signs from data
+          dataset.records.each(function(record) {
+            _.each(state.attributes.seriesFields, function(field) {
+              var fieldValue = record.get(field);
+              fieldValue = fieldValue.replace(/^\$/, '');
+              fieldValue = fieldValue.replace(/%$/, '');
+              record.set(field, fieldValue);
+            });
+          });
+
           dataset.queryState.set(state.get('queryState'));
           graph = new recline.View.nvd3[state.get('graphType')]({
             model: dataset,
