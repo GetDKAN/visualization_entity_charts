a10e24a322948d8958d32f1dcc0d4c0231556c59
Mariano Carballal <marianocarballal@gmail.com>
2015-07-28 16:53:20 -0300

    Fix resize when title is present

--- js/visualization_entity_charts_view.js
+++ js/visualization_entity_charts_view.js
@@ -10,21 +10,28 @@
       var state = $('.field-name-field-ve-settings .field-item:eq(0)').text();
       var $el;
       var title;
+      var height;
       var $body;
 
       if(state){
         state = new recline.Model.ObjectState(JSON.parse(state));
         $body = $(document.body);
+        $window = $(window);
         $body.removeClass('admin-menu');
 
         if($('#iframe-shell').length){
           $el = $('#iframe-shell');
           if(state.get('showTitle')){
             title = $el.find('h2 a').html();
-            $body.prepend('<h2>' + title + '</h2>');
+            $body.prepend('<h2 class="chartTitle">' + title + '</h2>');
+            height = getChartHeight(true);
+            resize();
+          } else {
+            height = getChartHeight(false);
           }
-          state.set('height', $(window).height());
-          state.set('width', $(window).width() - 10);
+          state.set('height', height);
+          state.set('width', $window.width() - 10);
+          $window.on('resize', resize);
         } else {
           $el = $('#graph');
           state.set('width', $('.field-name-field-ve-settings').width());
@@ -53,6 +60,24 @@
         }
         return url;
       }
+
+      function resize(){
+        var $title = $body.find('h2.chartTitle');
+        var hasTitle = !!$title.length;
+        $title.css({marginTop:'0px', padding:'20px', marginBottom:'0px'});
+        var height = getChartHeight(hasTitle);
+        $('.recline-nvd3').height(height);
+        $('#iframe-shell').height(height);
+
+      }
+      function getChartHeight(hasTitle) {
+        var height = (!hasTitle)
+          ? $(window).height()
+          : $(window).height() - $body.find('h2.chartTitle').outerHeight(true);
+
+        return height;
+      }
+
     }
   };
 })(jQuery);
