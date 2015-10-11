5bf254187e1b79577b55b7b2d85489dcac33a747
acouch <acinternets@gmail.com>
2015-10-11 05:00:56 +0000

    Enables multiple graphs on the same page

--- js/visualization_entity_charts_view.js
+++ js/visualization_entity_charts_view.js
@@ -6,54 +6,65 @@
 (function ($) {
   Drupal.behaviors.VisualizationEntityChartsView = {
     attach: function (context) {
-      var isIframe = !$('.content').is(':visible');
-      var state = $('.field-name-field-ve-settings .field-item:eq(0)').text();
-      var $el;
-      var title;
-      var height;
-      var $body;
 
-      if(state){
-        state = new recline.Model.ObjectState(JSON.parse(state));
-        $body = $(document.body);
-        $window = $(window);
-        $body.removeClass('admin-menu');
-
+      var graphs = $('.graph');
+      $.each(graphs, function(key, graph){
+        var $el = '#' + $(graph).attr('id');
+        var state = $($el).siblings(".entity").find(".field-name-field-ve-settings .field-item:eq(0)").text();
         if($('#iframe-shell').length){
           $el = $('#iframe-shell');
-          if(state.get('showTitle')){
-            title = $el.find('h2 a').html();
-            $body.prepend('<h2 class="chartTitle">' + title + '</h2>');
-            height = getChartHeight(true);
-            resize();
-          } else {
-            height = getChartHeight(false);
-          }
-          state.set('height', height);
-          state.set('width', $window.width() - 10);
-          $window.on('resize', resize);
-        } else {
-          $el = $('#graph');
-          state.set('width', $('.field-name-field-ve-settings').width());
         }
+        createGraph(state, $el);
+      });
 
-        var model = state.get('source');
-        var graph = null;
-        model.url = cleanURL(model.url);
-        $.get(model.url).done(function(data){
-          data = data.replace(/(?:\r|\n)/g, '\r\n');
-          data = CSV.parse(data);
-          state.set('model', new recline.Model.Dataset({records: data}));
-          model = state.get('model');
-          model.queryState.set(state.get('queryState'));
-          graph = new recline.View.nvd3[state.get('graphType')]({
-            model: model,
-            state: state,
-            el: $el
+
+
+      function createGraph(state,$el) {
+        var isIframe = !$('.content').is(':visible');
+        var title;
+        var height;
+        var $body;
+
+        if(state){
+          state = new recline.Model.ObjectState(JSON.parse(state));
+          $body = $(document.body);
+          $window = $(window);
+          $body.removeClass('admin-menu');
+
+          if($('#iframe-shell').length){
+            if(state.get('showTitle')){
+              title = $el.find('h2 a').html();
+              $body.prepend('<h2 class="chartTitle">' + title + '</h2>');
+              height = getChartHeight(true);
+              resize();
+            } else {
+              height = getChartHeight(false);
+            }
+            state.set('height', height);
+            state.set('width', $window.width() - 10);
+            $window.on('resize', resize);
+          } else {
+            state.set('width', $($el).width());
+          }
+
+          var model = state.get('source');
+          var graph = null;
+          model.url = cleanURL(model.url);
+          $.get(model.url).done(function(data){
+            data = data.replace(/(?:\r|\n)/g, '\r\n');
+            data = CSV.parse(data);
+            state.set('model', new recline.Model.Dataset({records: data}));
+            model = state.get('model');
+            model.queryState.set(state.get('queryState'));
+            graph = new recline.View.nvd3[state.get('graphType')]({
+              model: model,
+              state: state,
+              el: $el
+            });
+            graph.render();
           });
-          graph.render();
-        });
-        
+
+        }
       }
       function cleanURL(url){
         var haveProtocol = new RegExp('^(?:[a-z]+:)?//', 'i');
