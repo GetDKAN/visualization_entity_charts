a49b7904581ff416f3cfd0fa65555d8d38bf22db
voj <jeturian@gmail.com>
2017-06-30 09:44:45 +0800

    add proper handling of url if installation is in subdirectory

--- js/visualization_entity_charts.js
+++ js/visualization_entity_charts.js
@@ -110,7 +110,8 @@
           var re = /\[(.*?)\]/;
           var $sourceField = $('#control-chart-source');
           var uuid = re.exec($resourceField.val())[1];
-          $sourceField.val('/node/' + uuid + '/download');
+          // add proper handling of url if installation is in subdirectory
+          var source = Drupal.settings.basePath+'node/' + uuid + '/download';
         });
         sharedObject.state.on('change', function(){
           $('#edit-field-ve-settings-und-0-value').val(JSON.stringify(sharedObject.state.toJSON()));
